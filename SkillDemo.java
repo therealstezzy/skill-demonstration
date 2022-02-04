75b1e6b7c79ed0b3df0dab4063e82c0266a6788e
therealstezzy <97630660+therealstezzy@users.noreply.github.com>
2022-02-04 02:53:40 -0800

    fixed method

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,7 +1,7 @@
 public class SkillDemo{
   // This is where my beautiful code will go :)
   
-  public static int cubeNumber(int x){  // We will make this method purposely fail :(
-    return x * x * x + 1;
+  public static int cubeNumber(int x){  // Now the method works perfectly :)
+    return x * x * x ;
   }
 }
